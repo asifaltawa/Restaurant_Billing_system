4f551a9cfabe26a2b432fe94504a2a848f88c471
asif <mdasifaltawa@gmail.com>
2025-03-16 20:54:29 +0530

    change config

--- client/src/config.js
+++ client/src/config.js
@@ -1,7 +1,4 @@
-const isDevelopment = process.env.NODE_ENV === 'development';
 
 export const config = {
-  apiUrl: isDevelopment 
-    ? 'http://localhost:5000/api'
-    : 'https://restaurant-billing-system-backend.vercel.app/api'
+  apiUrl: 'https://restaurant-billing-system-backend.vercel.app/api'
 }; 
